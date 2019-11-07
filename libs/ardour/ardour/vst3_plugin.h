6ed8498bbde0072eb5cd55b989106b9f98437fff
Robin Gareus <robin@gareus.org>
2019-11-07 17:52:29 +0100

    VST3: Commence work on plugin-provide

--- libs/ardour/ardour/vst3_plugin.h
+++ libs/ardour/ardour/vst3_plugin.h
@@ -20,16 +20,140 @@
 #define _ardour_vst3_plugin_h_
 
 #include "pbd/signals.h"
-#include "vst3/vst3.h"
 
 #include "ardour/plugin.h"
+#include "ardour/vst3_host.h"
+
+namespace ARDOUR {
+class VST3PluginModule;
+}
+
+#if defined(__clang__)
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wnon-virtual-dtor"
+#elif __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#endif
+
+namespace Steinberg {
+
+/* VST3 hosted Plugin abstraction Implementation
+ *
+ * For convenience this is placed in the Steinberg namespace.
+ * Ardour::VST3Plugin has-a VST3PI (not is-a).
+ */
+class LIBARDOUR_API VST3PI : public Vst::IComponentHandler, public Vst::IConnectionPoint
+{
+public:
+	VST3PI (boost::shared_ptr<ARDOUR::VST3PluginModule> m, int index, std::string unique_id);
+	virtual ~VST3PI ();
+
+	/* IComponentHandler */
+	tresult beginEdit (Vst::ParamID id) SMTG_OVERRIDE;
+	tresult performEdit (Vst::ParamID id, Vst::ParamValue value) SMTG_OVERRIDE;
+	tresult endEdit (Vst::ParamID id) SMTG_OVERRIDE;
+	tresult restartComponent (int32 flags) SMTG_OVERRIDE;
+
+	/* IConnectionPoint API */
+	tresult connect (Vst::IConnectionPoint* other) SMTG_OVERRIDE;
+	tresult disconnect (Vst::IConnectionPoint* other) SMTG_OVERRIDE;
+	tresult notify (Vst::IMessage* message) SMTG_OVERRIDE;
+
+	tresult queryInterface (const TUID _iid, void** obj);
+	uint32 addRef () SMTG_OVERRIDE { return 1; }
+	uint32 release () SMTG_OVERRIDE { return 1; }
+
+	/* API for Ardour -- Ports */
+	uint32_t    designated_bypass_port () const { return _bypass_port_id; }
+	uint32_t    parameter_count () const { return _ctrl_params.size (); }
+	bool        parameter_is_automatable (uint32_t p) const { return _ctrl_params[p].automable; }
+	bool        parameter_is_readonly (uint32_t p) const { return _ctrl_params[p].read_only; }
+	std::string parameter_label (uint32_t p) const { return _ctrl_params[p].label; }
+	float       default_value (uint32_t p) const;
+	void        get_parameter_descriptor (uint32_t, ARDOUR::ParameterDescriptor&) const;
+	std::string print_parameter (uint32_t p) const;
+
+	uint32_t n_audio_inputs () const;
+	uint32_t n_audio_outputs () const;
+
+	/* API for Ardour -- Parameters */
+	void        set_parameter (uint32_t p, float value, int32 sample_off);
+	float       get_parameter (uint32_t p) const;
+	std::string format_parameter (uint32_t p) const;
+
+	/* API for Ardour -- Setup/Processing */
+	uint32_t  plugin_latency () { return _processor->getLatencySamples (); }
+	bool      set_block_size (int32_t);
+	bool      activate ();
+	bool      deactivate ();
+
+	Vst::ProcessContext& context () { return _context; }
+
+	void process (float** ins, float** outs, uint32_t n_samples);
+
+private:
+	void init ();
+	void terminate ();
+	bool connect_components ();
+	bool disconnect_components ();
+
+	bool  update_processor ();
+	int32 count_channels (Vst::MediaType, Vst::BusDirection, Vst::BusType) const;
+
+	Vst::ParamID index_to_id (uint32_t) const;
+
+	boost::shared_ptr<ARDOUR::VST3PluginModule> _module;
+
+	IPluginFactory*       _factory;
+	Vst::IComponent*      _component;
+	Vst::IEditController* _controller;
+
+	FUnknownPtr<Vst::IAudioProcessor> _processor;
+	Vst::ProcessContext               _context;
+
+	/* Parameters */
+	ParameterChanges _input_param_changes;
+	ParameterChanges _output_param_changes;
+
+	/* state */
+	bool    _is_processing;
+	int32_t _block_size;
+
+	/* ports */
+	struct Param {
+		uint32_t    id;
+		std::string label;
+		std::string unit;
+		int32_t     steps; // 1: toggle
+		double      normal;
+		bool        is_enum;
+		bool        read_only;
+		bool        automable;
+	};
+
+	uint32_t                         _bypass_port_id;
+	std::vector<Param>               _ctrl_params;
+	std::map<Vst::ParamID, uint32_t> _ctrl_id_index;
+	std::map<uint32_t, Vst::ParamID> _ctrl_index_id;
+	std::vector<float>               _shadow_data;
+
+	int _n_inputs;
+	int _n_outputs;
+	int _n_aux_inputs;
+	int _n_aux_outputs;
+	int _n_midi_inputs;
+	int _n_midi_outputs;
+};
+
+} // namespace Steinberg
 
 namespace ARDOUR {
 
 class LIBARDOUR_API VST3Plugin : public ARDOUR::Plugin
 {
 public:
-	VST3Plugin (AudioEngine&, Session&, std::string const&);
+	VST3Plugin (AudioEngine&, Session&, Steinberg::VST3PI*);
 	VST3Plugin (const VST3Plugin&);
 	~VST3Plugin ();
 
@@ -44,14 +168,19 @@ public:
 	float    get_parameter (uint32_t port) const;
 	int      get_parameter_descriptor (uint32_t which, ParameterDescriptor&) const;
 	uint32_t nth_parameter (uint32_t port, bool& ok) const;
+	bool     print_parameter (uint32_t, std::string&) const;
 
-	bool parameter_is_audio (uint32_t) const;
-	bool parameter_is_control (uint32_t) const;
+	bool parameter_is_audio (uint32_t) const { return false; }
+	bool parameter_is_control (uint32_t) const { return true; }
 	bool parameter_is_input (uint32_t) const;
 	bool parameter_is_output (uint32_t) const;
 
+	uint32_t designated_bypass_port ();
+
 	std::set<Evoral::Parameter> automatable () const;
 	std::string describe_parameter (Evoral::Parameter);
+	IOPortDescription describe_io_port (DataType dt, bool input, uint32_t id) const;
+	PluginOutputConfiguration possible_output () const;
 
 	std::string state_node_name () const { return "vst3"; }
 
@@ -62,9 +191,8 @@ public:
 	std::string do_save_preset (std::string);
 	void        do_remove_preset (std::string);
 
-	void activate ();
-	void deactivate ();
-	void cleanup ();
+	void activate ()   { _plug->activate (); }
+	void deactivate () { _plug->deactivate (); }
 
 	int set_block_size (pframes_t);
 
@@ -79,8 +207,13 @@ public:
 
 private:
 	samplecnt_t plugin_latency () const;
+	void        init ();
 	void        find_presets ();
+
+	Steinberg::VST3PI* _plug;
 };
+
+/* ****************************************************************************/
 
 class LIBARDOUR_API VST3PluginInfo : public PluginInfo
 {
@@ -90,6 +223,8 @@ public:
 
 	PluginPtr                         load (Session& session);
 	std::vector<Plugin::PresetRecord> get_presets (bool user_only) const;
+
+	boost::shared_ptr<VST3PluginModule> m;
 };
 
 } // namespace ARDOUR
